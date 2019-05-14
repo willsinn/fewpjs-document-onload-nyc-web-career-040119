4ad12352639e3dace8f9dd90b13c056021d60c09
William Sinn <w.h.sinn@gmail.com>
2019-05-13 22:28:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', () => {
+    updateText()
+});
+
+
+function updateText(){
+document.getElementById("text")
+.innerHTML = 'This is really cool!';
+}
